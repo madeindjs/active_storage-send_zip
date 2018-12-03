59bbef60dd72001afd5a75f420a466e576f79c88
Alexandre Rousseau <contact@rousseau-alexandre.Fr>
2018-12-03 13:19:47 +0100

    Refactor test

--- test/active_storage/send_zip_test.rb
+++ test/active_storage/send_zip_test.rb
@@ -31,10 +31,7 @@ class ActiveStorage::SendZipTest < Minitest::Test
       ActiveStorageMock.new('foo.txt')
     ]
 
-    controller = FakeController.new
-    files_saved = controller.test_save_files_on_server(files)
-    assert_equal 1, files_saved.count
-    refute_nil controller.test_create_temporary_zip_file(files_saved)
+    assert_produce_files files
   end
 
   def test_it_should_save_two_active_support
@@ -43,9 +40,24 @@ class ActiveStorage::SendZipTest < Minitest::Test
       ActiveStorageMock.new('bar.txt')
     ]
 
+    assert_produce_files files, count: 2
+  end
+
+  # def test_it_should_save_two_active_support
+  #   files = [
+  #     ActiveStorageMock.new('foo.txt'),
+  #     ActiveStorageMock.new('foo.txt')
+  #   ]
+  #
+  #   assert_produce_files files, count: 2
+  # end
+
+  private
+
+  def assert_produce_files(files, count: 1)
     controller = FakeController.new
     files_saved = controller.test_save_files_on_server(files)
-    assert_equal 2, files_saved.count
+    assert_equal count, files_saved.count
     refute_nil controller.test_create_temporary_zip_file(files_saved)
   end
 end
